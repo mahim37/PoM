8898c7b7dee8fb00bbfc365a1a9b1134b2d89875
Mahim <mahim.g03@gmail.com>
2024-12-02 08:01:53 +0530

    allowed all origins

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { getScore } from './score.mjs';
 
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true 
+  }));
 const PORT = 8000;
 const DEFAULT_PRICE1 = 50;
 const DEFAULT_PRICE2 = 30;
